78f20c3f42249b5458e6ac42238779b0f35f5831
Alvin Januar <dev@alvinjanuar.com>
2025-06-20 00:37:53 +0800

    chore: change neovide transparency to opacity

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 if vim.g.neovide then
   -- scale_factor
   -- vim.g.neovide_scale_factor = 0.7
-  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_opacity = 0.8
   vim.g.neovide_cursor_vfx_mode = 'pixiedust'
   vim.g.neovide_cursor_vfx_particle_density = 100.0
   vim.g.neovide_cursor_vfx_particle_lifetime = 4.0
